aec353651f5fbabe40781ec3c21e20aa0816e39b
MrSaqib2002 <saqibjahangir200@gmail.com>
2025-04-10 12:33:20 +0500

    my new work

---
+++ lib/Screens/NewLoginScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewLoginScreen extends StatelessWidget {
+  const NewLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
